a585f5a43ec3e6418a77a4b6fed3e0e4b9762393
Ankur Dome <ankurdome02@gmail.com>
2024-07-22 01:52:27 +0530

    added template outline in grid

---
+++ src/routes/(tools)/document-generator/+page.svelte
@@ -0,0 +1,59 @@
+<script>
+    
+</script>
+
+<div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-1 overflow-hidden rounded-lg">
+    <div class="w-full h-full dark:text-white flex flex-col">
+        <h1 class="text-center">Devstar Document Templates Module</h1>
+        <div class="flex">
+            <div class="w-[15%] border border-yellow-100">Left Side Navbar
+                <nav>
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                        <li>4</li>
+                        <li>5</li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="w-[70%] flex gap-10 border border-yellow-100">
+                <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-1 overflow-hidden rounded-lg">
+                    <section class="tmeplate-section">
+                    <h1 class="text-xl font-bold mb-6 text-gray-800">Templates</h1>
+                    <div class="flex overflow x-auto gap-4">
+                        <div class="group relative w-64 h-80 rounded-lg overflow-hidden bg-gray-900 cursor pointer shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1">
+                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="" alt="Template 1">
+                            <div class="absolute inset-0 px-4 py-2 bottom-0 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-gradient-to-b from-gray-900 to-transparent">
+                                <h3 class="text-lg font-bold text-white mb-2">Dota 2</h3>
+                                <p class="text-gray-300">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptro tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus</p>
+                              </div>
+                        </div>
+                        <div class="group relative w-64 h-80 rounded-lg overflow-hidden bg-gray-900 cursor pointer shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1">
+                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="" alt="Template 1">
+                            <div class="absolute inset-0 px-4 py-2 bottom-0 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-gradient-to-b from-gray-900 to-transparent">
+                                <h3 class="text-lg font-bold text-white mb-2">Dota 2</h3>
+                                <p class="text-gray-300">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptro tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus</p>
+                              </div>
+                        </div>
+                        <div class="group relative w-64 h-80 rounded-lg overflow-hidden bg-gray-900 cursor pointer shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1">
+                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="" alt="Template 1">
+                            <div class="absolute inset-0 px-4 py-2 bottom-0 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-gradient-to-b from-gray-900 to-transparent">
+                                <h3 class="text-lg font-bold text-white mb-2">Dota 2</h3>
+                                <p class="text-gray-300">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptro tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus i</p>
+                              </div>
+                        </div>
+                    </div>
+                    </section>
+                    </div>
+            </div>
+            <div class="w-[15%] border border-yellow-100">Right Side Search bar with some text</div>
+        </div>
+    </div>
+</div>
+
+
+
+<style>
+    
+</style>
