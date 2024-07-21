b54867ab507975eb76594c9098943a7dbfd140e4
Ankur Dome <ankurdome02@gmail.com>
2024-07-22 02:03:00 +0530

    Update +page.svelte

--- src/routes/(tools)/document-generator/+page.svelte
+++ src/routes/(tools)/document-generator/+page.svelte
@@ -23,21 +23,21 @@
                     <h1 class="text-xl font-bold mb-6 text-gray-800">Templates</h1>
                     <div class="flex overflow x-auto gap-4">
                         <div class="group relative w-64 h-80 rounded-lg overflow-hidden bg-gray-900 cursor pointer shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1">
-                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="" alt="Template 1">
+                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="https://picsum.photos/seed/picsum/200/300" alt="Template 1">
                             <div class="absolute inset-0 px-4 py-2 bottom-0 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-gradient-to-b from-gray-900 to-transparent">
                                 <h3 class="text-lg font-bold text-white mb-2">Dota 2</h3>
                                 <p class="text-gray-300">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptro tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus</p>
                               </div>
                         </div>
                         <div class="group relative w-64 h-80 rounded-lg overflow-hidden bg-gray-900 cursor pointer shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1">
-                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="" alt="Template 1">
+                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="https://picsum.photos/seed/picsum/200/300" alt="Template 1">
                             <div class="absolute inset-0 px-4 py-2 bottom-0 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-gradient-to-b from-gray-900 to-transparent">
                                 <h3 class="text-lg font-bold text-white mb-2">Dota 2</h3>
                                 <p class="text-gray-300">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptro tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus</p>
                               </div>
                         </div>
                         <div class="group relative w-64 h-80 rounded-lg overflow-hidden bg-gray-900 cursor pointer shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1">
-                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="" alt="Template 1">
+                            <img class="w-full h-full object-cover group-hover:opacity-50 transition duration-300 ease-in-out t" src="https://picsum.photos/seed/picsum/200/300" alt="Template 1">
                             <div class="absolute inset-0 px-4 py-2 bottom-0 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out bg-gradient-to-b from-gray-900 to-transparent">
                                 <h3 class="text-lg font-bold text-white mb-2">Dota 2</h3>
                                 <p class="text-gray-300">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptro tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus i</p>
@@ -45,7 +45,7 @@
                         </div>
                     </div>
                     </section>
-                    </div>
+                </div>
             </div>
             <div class="w-[15%] border border-yellow-100">Right Side Search bar with some text</div>
         </div>
